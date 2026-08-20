a4d122f7d5a0ed0d4ae84fa73954d196380b0624
sahibahmed <sahib.ahmed006@gmail.com>
2026-08-19 21:03:22 -0400

    Add colored object mask

--- tests/camera_test.py
+++ tests/camera_test.py
@@ -1,9 +1,10 @@
 #GOAL
-#To capture image usng Pi Camera and store it in array
+#To detect an colored object
 
 import cv2
 from picamera2 import Picamera2
 import time
+import numpy as np
 #from adafruit_servokit import ServoKit
 
 """
@@ -24,10 +25,23 @@ picam2.start() #avoiding using showpreview=True as I am using SSH
 time.sleep(1)
 array = picam2.capture_array("main") #capture in preview mode and store to array
 
-array = cv2.cvtColor(array, cv2.COLOR_RGBA2BGR) #conviret Picamera2 RGB to OpenCV BGR
+array = cv2.cvtColor(array, cv2.COLOR_RGBA2BGR) #convert Picamera2 RGB to OpenCV BGR'
+array = cv2.cvtColor(array, cv2.COLOR_BGR2HSV) #convert to HSV format for color detection
 
+# standard value for detecting the color blue 
+lower_limit = np.array([100, 50, 50])
+upper_limit = np.array([130, 255, 255])
+
+mask = cv2.inRange(array, lower_limit, upper_limit) #defined range for blue 
+
+array = cv2.bitwise_and(array, array, mask=mask) #only 
+
+
+
+"""
 print(type(array))
 print(array.shape) # printed results : (480, 640, 4) 
+"""
 
 height = array.shape[0] # 480 height 
 width = array.shape[1] # 640 width
@@ -35,8 +49,10 @@ width = array.shape[1] # 640 width
 center_x = int(width/2) 
 center_y = int(height/2)
 
+"""
 print(center_x)
 print(center_y)
+"""
 
 cv2.circle(array, (center_x, center_y), 4, (0,0,255), -1)
 
